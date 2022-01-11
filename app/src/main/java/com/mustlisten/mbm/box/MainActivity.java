8d304de7ccd569cfd3e940f75cf9eb1eb69e2c85
wuliang6661@163.com <wy19941007>
2022-01-11 11:28:41 +0800

    first commit

---
+++ app/src/main/java/com/mustlisten/mbm/box/MainActivity.java
@@ -0,0 +1,51 @@
+package com.mustlisten.mbm.box;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startVoice();
+    }
+
+
+    private MediaPlayer mediaPlayer;
+
+    public void startVoice() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            return;
+        }
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource("https://cdn.mustlisten.com/qq_music/%E8%90%8C%E8%90%8C%E5%93%92%E5%A4%A9%E5%9B%A2%2F%E6%8B%9C%E5%B9%B4%E6%AD%8C%2F16641083.mp3");
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mediaPlayer.start();
+                    mediaPlayer.setLooping(true);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopVoice() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release(); //切记一定要release
+            mediaPlayer = null;
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+password:mustlisten
+
+alias:mbm
+password:mustlisten
+
+name:mbm
+org unit:mbm
+org:mbm
+city:sh
+province:sh
+country:cn
